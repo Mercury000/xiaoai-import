a882be9722600dcfd807e0479d92ba646823d838
Mercury <mercury.me@qq.com>
2026-07-29 22:18:23 +0800

    Revert "Add WakeUp import dialog"
    This reverts commit c990a7fb7cfb317745c725044938d545da6b8503.

--- app/src/main/java/me/padi/xiaoai/screen/JwSystemScreen.kt
+++ app/src/main/java/me/padi/xiaoai/screen/JwSystemScreen.kt
@@ -30,7 +30,6 @@ import com.kongzue.dialogx.dialogs.TipDialog
 import com.kongzue.dialogx.dialogs.WaitDialog
 import com.mercury.xiaoaiimport.click.openAiImportScreen
 import com.mercury.xiaoaiimport.click.openJsonImportDialog
-import com.mercury.xiaoaiimport.click.openWakeUpImportDialog
 import com.mercury.xiaoaiimport.HostCompat
 import com.mercury.xiaoaiimport.OkHttpClientManager
 import com.mercury.xiaoaiimport.ShiguangAdapterEntry
@@ -329,14 +328,6 @@ class JwSystemScreen : BaseActivity() {
     private fun buildBuiltinImportItems(): List<JwItem> {
         return listOf(
             JwItem(
-                name = "WakeUp导入",
-                type = JwType.COMMON,
-                extra = "__wakeup_import__",
-                isCommon = true,
-                sortKey = "#",
-                order = 9_999
-            ),
-            JwItem(
                 name = "JSON导入",
                 type = JwType.COMMON,
                 extra = "__json_import__",
@@ -463,10 +454,6 @@ class JwSystemScreen : BaseActivity() {
     private fun handleItemClick(context: Context, item: JwItem) {
         when (item.type) {
             JwType.COMMON, JwType.SCHOOL -> {
-                if (item.extra == "__wakeup_import__") {
-                    openWakeUpImportDialog(context)
-                    return
-                }
                 if (item.extra == "__json_import__") {
                     openJsonImportDialog(context)
                     return
